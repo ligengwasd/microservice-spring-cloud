9dd4dc1634f27cb6a242207ac34975e61cd7b9e8
李赓 <ligeng@1dabang.cn>
2017-02-21 01:45:23 +0800

    batch

--- microservice-batch/src/main/java/com/itmuch/MicroserviceBatchApplication.java
+++ microservice-batch/src/main/java/com/itmuch/MicroserviceBatchApplication.java
@@ -1,12 +1,26 @@
 package com.itmuch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableTask
 public class MicroserviceBatchApplication {
-
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return new HelloWorldCommandLineRunner();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceBatchApplication.class, args);
+	}
+	public static class HelloWorldCommandLineRunner implements CommandLineRunner {
+
+		@Override
+		public void run(String... strings) throws Exception {
+			System.out.println("Hello World!");
+		}
 	}
 }
