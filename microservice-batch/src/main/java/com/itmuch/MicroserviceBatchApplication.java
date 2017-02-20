8b8a52d2da3a7a5621fb4f3b4da5564aa4bbf1c3
李赓 <ligeng@1dabang.cn>
2017-02-21 01:31:46 +0800

    batch

---
+++ microservice-batch/src/main/java/com/itmuch/MicroserviceBatchApplication.java
@@ -0,0 +1,12 @@
+package com.itmuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBatchApplication.class, args);
+	}
+}
